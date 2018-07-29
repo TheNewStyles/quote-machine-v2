d28c47dfa5c52c530f35e06b91a9a4e71adf477f
TheNewStyles <tyler.peterson@utah.edu>
2018-07-29 14:49:39 -0500

    Create a React app and publish it to GitHub Pages

--- src/__tests__/Author.test.js
+++ src/__tests__/Author.test.js
@@ -1,9 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
 import Author from '../Components/Author';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<Author />, div);
-  ReactDOM.unmountComponentAtNode(div);
+describe('<Author />', () => {
+  it('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<Author />, div);
+    ReactDOM.unmountComponentAtNode(div);
+  });
+
+  it('renders correct author passed in as prop', () => {
+    const div = document.createElement('div');
+    const expected = "TEST_AUTHOR";
+    ReactDOM.render(<Author author={expected} />, div);
+    expect(div.firstChild.textContent).toEqual(expected);
+    ReactDOM.unmountComponentAtNode(div);
+  });
 });
--- src/__tests__/NewQuoteButton.test.js
+++ src/__tests__/NewQuoteButton.test.js
@@ -1,9 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
 import NewQuoteButton from '../Components/NewQuoteButton';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<NewQuoteButton />, div);
-  ReactDOM.unmountComponentAtNode(div);
+describe('<NewQuoteButton />', () => {
+  it('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<NewQuoteButton />, div);
+    ReactDOM.unmountComponentAtNode(div);
+  });
+
+  it('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<NewQuoteButton />, div);
+    const newQuoteButton = div.firstChild;
+    const expected = "New Quote";
+    expect(newQuoteButton.textContent).toEqual(expected);
+    ReactDOM.unmountComponentAtNode(div);
+  });  
 });
