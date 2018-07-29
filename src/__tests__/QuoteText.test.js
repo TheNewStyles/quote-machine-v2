477b4bc5feb8a06cce181bf789d682fb065d9fdd
TheNewStyles <tyler.peterson@utah.edu>
2018-07-29 16:03:18 -0500

    Updates

--- src/__tests__/QuoteText.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import QuoteText from '../Components/QuoteText';
-
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<QuoteText />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
--- src/__tests__/TweetButton.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import TweetButton from '../Components/TweetButton';
-
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<TweetButton />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
